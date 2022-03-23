27a63ccc33893cb0121c42a768080a922f4c7da8
git <x@mwni.io>
2022-03-23 15:57:49 +0100

    swapped model prompt context/command order

--- mediator/strings.py
+++ mediator/strings.py
@@ -3,7 +3,7 @@ user_prompts = {
 }
 
 model_prompts = {
-    'generate_questions': 'Generate a list of questions that an attorney would ask their client about this legal lawsuit.\n\n"""\n{complaint}\n"""',
-    'summarize_complaint': 'Generate a very long detailed summary about the Plaintiff\'s legal complaint from this conversation:\n\n"""\n{inquiries}\n"""',
+    'generate_questions': '"""\n{complaint}\n"""\n\nGenerate a list of questions that an attorney would ask their client about this legal lawsuit.',
+    'summarize_complaint': '"""\n{inquiries}\n"""\n\nGenerate a very long detailed summary about the Plaintiff\'s legal complaint from this conversation.',
     'inquiry_block': 'Lawyer: {lawyer}\nPlaintiff: {plaintiff}'
 }
